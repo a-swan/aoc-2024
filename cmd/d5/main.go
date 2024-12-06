ebd7a6f27b85217aa52e1ea789274b6a23b708c7
alexpswanson <alexpswanson@gmail.com>
2024-12-06 11:54:51 +0200

    follow up day

---
+++ cmd/d5/main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+  "os"
+  "log"
+  "fmt"
+  "strings"
+  "slices"
+  "strconv"
+
+  "github.com/a-swan/aoc-2024/pkg"
+)
+
+type Page struct {
+  parent []int
+  child []int
+}
+
+func part1(lines []string) {
+  fmt.Printf("Starting part 1...\n")
+  rules := make(map[int]Page)
+
+  // build some sort of search tree of rules
+  var i int
+  for i = 0; i < len(lines); i++ {
+    if lines[i] == "" {
+      break
+    }
+
+    var a, b int
+    _, err := fmt.Sscanf(lines[i], "%d|%d", &a, &b)
+    if err != nil {
+      log.Fatal(err)
+    }
+
+    tmpA := rules[a]
+    tmpA.child = append(tmpA.child, b)
+
+    tmpB := rules[b]
+    tmpB.parent = append(tmpB.parent, a)
+
+    rules[a] = tmpA
+    rules[b] = tmpB
+  }
+  // when the empty row hits
+  fmt.Printf("i = %d\n", i)
+
+  fmt.Printf("%v\n", rules)
+  
+  for i = i+1; i < len(lines); i++ {
+    pageOrder := strings.Split(lines[i], ",")
+
+    left := make([]int, 0)
+    fails := false
+    for _, tmpPage := range pageOrder {
+      page, _ := strconv.Atoi(tmpPage)
+      for _, previousPage := range left {
+        if slices.Contains(rules[page].child, previousPage) {
+          fmt.Printf("%s - fails!\n", pageOrder)
+          fails = true
+          break
+        }
+      }
+      if fails {
+        break
+      }
+      left = append(left, page)
+    }
+    fmt.Printf("%s - passes! \n", pageOrder)
+    fmt.Printf("Left: %v\n", left)
+  }
+}
+
+func main() {
+  var filePath string
+  
+  if len(os.Args) > 1 {
+    filePath = os.Args[1]
+  } else {
+    filePath = "input.txt"
+  }
+
+  lines, err := pkg.ReadFile(filePath)
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  part1(lines)
+}
